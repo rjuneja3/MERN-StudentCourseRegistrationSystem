809d7c95cfe61a37c24f7266b3229b659a86e330
Dinara <inraidi@gmail.com>
2021-03-23 17:39:17 -0400

    Created and set up Models: Student and Course

---
+++ DinaraSharipova_comp308_Lab3/app/models/course.server.model.js
@@ -0,0 +1,36 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CourseSchema = new Schema({
+    courseCode: {
+        type: String,
+        required: 'Course code is required',
+        trim: true,
+        default: '',
+        min:3,
+        max: 40
+    },
+    courseName: {
+        type: String,
+        trim: true,
+        min:5,
+        max:40,
+        default: '',
+        required: 'Course Name is required'
+    },
+    section: {
+        type: String,
+        trim: true,
+        default: '',
+        required: 'Section is required'
+    },
+    semester: {
+        type: Number,
+        default: 1,
+        required: 'Semester is required'
+    },
+    studentEntity: {
+        type: Schema.Types.ObjectId,
+        ref: 'Student'
+    },
+});
+mongoose.model('Course', CourseSchema);
---
+++ DinaraSharipova_comp308_Lab3/app/models/student.server.model.js
@@ -0,0 +1,95 @@
+﻿const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const Schema = mongoose.Schema;
+
+var StudentSchema = new Schema({
+	firstName: {
+		type: String,
+		required: 'First Name is required',
+		max: 40
+	},
+	lastName: {
+		type: String,
+		required: 'Last Name is required',
+		max: 40
+	},
+	studentNumber: {
+		type: Number,
+		required: 'Student Number is required',
+		unique: true,
+		max: 15,
+		trim: true
+	},
+	email: {
+		type: String,
+		trim: true,
+		unique: true,
+		// Validate the email format
+		match: [/.+\@.+\..+/, "Please fill a valid email address"]
+	},
+	password: {
+		type: String,
+		unique: true,
+		validate: [
+			(password) => password && password.length > 6,
+			'Password should be longer'
+		]
+	},
+	address:{
+		type: String,
+		required: 'Address is required',
+		max: 100
+	},
+	city:{
+		type: String,
+		required: 'City is required',
+		max: 50
+	},
+	phoneNumber:{
+		type: Number,
+		required: 'Phone Number is required',
+		max: 12,
+		min:9
+	},
+	program:{
+		type: String,
+		required: 'Program is required',
+		max: 100
+	}
+});
+
+// Set the 'fullname' virtual property
+StudentSchema.virtual('fullName').get(function() {
+	return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+	const splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+// Use a pre-save middleware to hash the password
+// before saving it into database
+StudentSchema.pre('save', function(next){
+	//hash the password before saving it
+	this.password = bcrypt.hashSync(this.password, saltRounds);
+	next();
+});
+
+// Create an instance method for authenticating student
+StudentSchema.methods.authenticate = function(password) {
+	//compare the hashed password of the database 
+	//with the hashed version of the password the student enters
+	return this.password === bcrypt.hashSync(password, saltRounds);
+};
+
+
+
+StudentSchema.set('toJSON', {
+	getters: true,
+	virtuals: true
+});
+
+
+mongoose.model('Student', StudentSchema);
